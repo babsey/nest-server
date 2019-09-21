a187dd027dd335bfdb3cef3a662fe9f7e6bb1e64
Sebastian Spreizer <spreizer@web.de>
2019-09-21 13:58:25 +0200

    Update conf for docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import sphinx_rtd_theme
-
 
 # -- Project information -----------------------------------------------------
 
@@ -30,7 +28,6 @@ author = 'Sebastian Spreizer'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
