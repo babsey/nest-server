2a6762e8a63d0028668de9c47a9f3ce8657b61f4
Sebastian Spreizer <spreizer@web.de>
2020-01-21 16:56:01 +0100

    Fix NodeCollection arguments

--- nest_server/api/client.py
+++ nest_server/api/client.py
@@ -8,30 +8,43 @@ __all__ = [
 ]
 
 
+def NodeCollection(kwargs):
+  if 'nodes' in kwargs:
+    kwargs['nodes'] = nest.NodeCollection(kwargs['nodes'])
+  if 'source' in kwargs:
+    kwargs['source'] = nest.NodeCollection(kwargs['source'])
+  if 'target' in kwargs:
+    kwargs['target'] = nest.NodeCollection(kwargs['target'])
+  if 'pre' in kwargs:
+    kwargs['pre'] = nest.NodeCollection(kwargs['pre'])
+  if 'post' in kwargs:
+    kwargs['post'] = nest.NodeCollection(kwargs['post'])
+  return kwargs
+
+
 @get_or_error
 def api_client(request, call, data, *args, **kwargs):
 
-    if callable(call):
-        data['request']['call'] = call.__name__
+  if callable(call):
+    data['request']['call'] = call.__name__
 
-        if str(kwargs.get('return_doc', 'false')) == 'true':
-            response = call.__doc__
-        elif str(kwargs.get('return_source', 'false')) == 'true':
-            response = inspect.getsource(call)
-        else:
-            if call.__name__ == 'SetKernelStatus':
-                kernelStatus = nest.GetKernelStatus()
-                for paramKey, paramVal in kwargs['params'].items():
-                    kwargs['params'][paramKey] = type(kernelStatus[paramKey])(paramVal)
-            elif call.__name__ == 'SetStatus':
-                status = nest.GetStatus(kwargs['nodes'])
-                for paramKey, paramVal in kwargs['params'].items():
-                    kwargs['params'][paramKey] = type(status[paramKey])(paramVal)
-            response = nest.hl_api.serializable(call(*args, **kwargs))
+    if str(kwargs.get('return_doc', 'false')) == 'true':
+      response = call.__doc__
+    elif str(kwargs.get('return_source', 'false')) == 'true':
+      response = inspect.getsource(call)
     else:
-        response = call
+      kwargs = NodeCollection(kwargs)
+      if call.__name__ == 'SetKernelStatus':
+        kernelStatus = nest.GetKernelStatus()
+        for paramKey, paramVal in kwargs['params'].items():
+          kwargs['params'][paramKey] = type(kernelStatus[paramKey])(paramVal)
+      elif call.__name__ == 'SetStatus':
+        status = nest.GetStatus(kwargs['nodes'])
+        for paramKey, paramVal in kwargs['params'].items():
+          kwargs['params'][paramKey] = type(status[paramKey])(paramVal)
+      response = call(*args, **kwargs)
+  else:
+    response = call
 
-    data['response']['data'] = response
-    print(response)
-
-    return data
+  data['response']['data'] = nest.hl_api.serializable(response)
+  return data
