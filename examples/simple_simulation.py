3c913c154ed60118aee21373c6bd6b3fa7160dfa
Sebastian Spreizer <spreizer@web.de>
2020-01-21 21:28:01 +0100

    Update examples

--- examples/simple_simulation.py
+++ examples/simple_simulation.py
@@ -4,15 +4,22 @@ import json
 
 def nest_api(call, url='http://localhost:5000/api/nest/', *args, **kwargs):
   headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-  data = requests.post(url + call, json.dumps(kwargs), headers=headers).json()
+  req = requests.post(url + call, json.dumps(kwargs), headers=headers)
+  data = req.json()
   response = data['response']
-  print('%s ... %s' %(call, response['status']))
+  elapsed = req.elapsed.total_seconds()
+  unit = 's'
+  if elapsed < 1000:
+    elapsed *= 1000
+    unit = 'ms'
+  print('{:11s} ... {} \t{:.1f} {}'.format(call, response['status'], elapsed, unit))
   if (response['status'] == 'error'):
-    print(' - %s' %response['msg'])
+    print(' - {}'.format(response['msg']))
   return response['data']
 
 
 
+print(35*'-')
 nest_api('ResetKernel')
 neurons = nest_api('Create', model="iaf_psc_alpha", n=10)
 pg = nest_api('Create', model="poisson_generator", params={"rate": 10.})
@@ -23,4 +30,6 @@ nest_api('Connect', pre=vm, post=neurons)
 
 nest_api('Simulate', t=1000.)
 events = nest_api('GetStatus', nodes=vm, keys='n_events')[0]
+
+print(35*'-')
 print('Number of events:', events)
