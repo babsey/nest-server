abb470d63498ac7ebd706588cf01dccdfc97f245
Sebastian Spreizer <spreizer@web.de>
2020-03-31 11:26:01 +0200

    Update nest_server

--- nest_server/main.py
+++ nest_server/main.py
@@ -4,17 +4,15 @@ import datetime
 import inspect
 
 import flask
-from flask import Flask, request, jsonify
+from flask import json, Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 
 import nest
-try:
-  nest.Install("nestmlmodule")
-except ImportError:
-  pass
+import numpy as np
 
-from .api import initializer as api_init
+from .api.initializer import init_data, get_arguments
 from .api.client import api_client
+from .api.capture import Capturing
 from . import scripts
 
 from . import __version__
@@ -34,7 +32,8 @@ nest_calls.sort()
 
 @app.route('/', methods=['GET'])
 def index():
-  response = {
+  data = init_data(request)
+  data['response'] = {
       'server': {
           'version': __version__,
           'git': {
@@ -43,10 +42,67 @@ def index():
           }
       },
       'simulator': {
-          'version': nest.version().split('-')[1],
+          'version': nest.version(),
       },
+      'status': 'ok'
   }
-  return jsonify(response)
+  return jsonify(data)
+
+
+# --------------------------
+# exec
+# --------------------------
+
+@app.route('/exec', methods=['GET', 'POST'])
+@cross_origin()
+def nest_exec():
+  data = init_data(request)
+  args, kwargs = get_arguments(request, data)
+  with Capturing() as stdout:
+    try:
+      source = kwargs.get('source', '')
+      globals = {'__builtins__': None}
+      locals = {
+        'list': list,
+        'nest': nest,
+        'np': np,
+        'print': print,
+        'set': set,
+      }
+      exec(source, globals, locals)
+      if 'return' in kwargs:
+        if isinstance(kwargs['return'], list):
+          response_data = {}
+          for variable in kwargs['return']:
+            response_data[variable] = locals.get(variable, None)
+        else:
+          response_data = locals.get(kwargs['return'], None)
+        response_serialized = nest.hl_api.serializable(response_data)
+        response_json = json.dumps(response_serialized)
+        data['response']['data'] = response_serialized
+      data['response']['status'] = 'ok'
+    except nest.kernel.NESTError as e:
+      print('NESTError:', e)
+      data['response'] = {
+          'data': None,
+          'error': {
+              'name': getattr(e, 'errorname'),
+              'message': getattr(e, 'errormessage').split(':')[-1]
+          },
+          'status': 'error'
+      }
+    except Exception as e:
+      print(e)
+      data['response'] = {
+          'data': None,
+          'error': {
+              'name': 'Error',
+              'message': str(e)
+          },
+          'status': 'error'
+      }
+  data['response']['stdout'] = '\n'.join(stdout)
+  return jsonify(data)
 
 
 # --------------------------
@@ -56,23 +112,36 @@ def index():
 @app.route('/api/nest', methods=['GET'])
 @cross_origin()
 def router_nest():
-  data, args, kwargs = api_init.data_and_args(request)
-  response = api_client(request, nest_calls, data)
-  return jsonify(response)
+  data = init_data(request)
+  args, kwargs = get_arguments(request, data)
+  api_client(request, call, data, *args, **kwargs)
+  data['response']['status'] = 'ok'
+  return jsonify(**data)
 
 
 @app.route('/api/nest/<call>', methods=['GET', 'POST'])
 @cross_origin()
 def router_nest_call(call):
-  data, args, kwargs = api_init.data_and_args(request, call)
+  data = init_data(request, call)
+  args, kwargs = get_arguments(request, data)
   if call in nest_calls:
     call = getattr(nest, call)
-    response = api_client(request, call, data, *args, **kwargs)
+    api_client(request, call, data, *args, **kwargs)
+    data['response']['status'] = 'ok'
   else:
-    data['response']['msg'] = 'The request cannot be called in NEST.'
-    data['response']['status'] = 'error'
-    response = data
-  return jsonify(response)
+    data['response'] = {
+        'error': 'The request cannot be called in NEST.',
+        'status': 'error'
+    }
+
+  data_json = json.dumps(data)
+  data_json = data_json.replace('Infinity', '1e+9999')
+  response = app.response_class(
+      response=data_json,
+      status=200,
+      mimetype='application/json'
+  )
+  return response
 
 
 # --------------------------
@@ -82,83 +151,103 @@ def router_nest_call(call):
 @app.route('/script/<filename>/<call>', methods=['POST', 'OPTIONS'])
 @cross_origin()
 def script(filename, call):
-  # print(request.get_json())
+  data = init_data(request, call)
   try:
     script = getattr(scripts, filename)
     func = getattr(script, call)
-    response = func(request.get_json())
+    data['response'] = func(request.get_json())
+    data['response']['status'] = 'ok'
   except nest.kernel.NESTError as e:
-    print(e)
-    error = {
-        'name': getattr(e, 'errorname'),
-        'message': getattr(e, 'errormessage').split(':')[-1]
+    print('NESTError:', e)
+    data['response'] = {
+        'error': {
+            'name': getattr(e, 'errorname'),
+            'message': getattr(e, 'errormessage').split(':')[-1]
+        },
+        'status': 'error'
     }
-    response = {'error': error}
   except Exception as e:
     print(e)
-    error = {
-        'name': 'Error',
-        'message': str(e)
+    data['response'] = {
+        'error': {
+            'name': 'Error',
+            'message': str(e)
+        },
+        'status': 'error'
     }
-    response = {'error': error}
-  return jsonify(response)
+  return jsonify(data)
 
 
 @app.route('/source', methods=['GET'])
 @cross_origin()
 def inspect_files():
+  data = init_data(request)
   try:
     source = inspect.getsource(scripts)
-    response = {
+    data['response'] = {
         'source': source,
+        'status': 'ok'
     }
   except Exception as e:
     print(e)
-    error = {
-        'name': 'Error',
-        'message': str(e)
+    data['response'] = {
+        'error': {
+            'name': 'Error',
+            'message': str(e)
+        },
+        'status': 'error'
     }
-    response = {'error': error}
-  return jsonify(response)
+  return jsonify(data)
 
 
 @app.route('/source/<filename>', methods=['GET'])
 @cross_origin()
 def inspect_script(filename):
+  data = init_data(request)
+  data['request']['filename'] = filename
   try:
     script = getattr(scripts, filename)
     source = inspect.getsource(script)
-    response = {
+    data['response'] = {
         'source': source,
+        'status': 'ok'
     }
   except Exception as e:
     print(e)
-    error = {
-        'name': 'Error',
-        'message': str(e)
+    data['response'] = {
+        'error': {
+            'name': 'Error',
+            'message': str(e)
+        },
+        'status': 'error'
     }
-    response = {'error': error}
-  return jsonify(response)
+  return jsonify(data)
 
 
 @app.route('/source/<filename>/<call>', methods=['GET'])
 @cross_origin()
 def inspect_func(filename, call):
+  data = init_data(request, call)
+  args, kwargs = get_arguments(request, data)
+  data['request']['filename'] = filename
   try:
     script = getattr(scripts, filename)
     func = getattr(script, call)
     source = inspect.getsource(func)
-    response = {
+    data['response'] = {
         'source': source,
+        'status': 'ok'
     }
   except Exception as e:
     print(e)
-    error = {
-        'name': 'Error',
-        'message': str(e)
+    data['response'] = {
+        'error': {
+            'name': 'Error',
+            'message': str(e)
+        },
+        'status': 'error'
     }
-    response = {'error': error}
-  return jsonify(response)
+  return jsonify(data)
 
 
 if __name__ == "__main__":
